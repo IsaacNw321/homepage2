436a9ff51b5c68b35446404ab0125e725bcc88cb
Usuario <135558153+IsaacNw321@users.noreply.github.com>
2025-04-23 10:48:08 -0400

    update: index.css

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,8 +7,8 @@ export default function Hero() {
   }
   return (
     <section
-  className="w-full h-full m-auto flex justify-center my-8 sm:-mx-0 lg:-mx-0 md:w-auto md:h-96 lg:w-full lg:h-[calc(100vh-112px)]">
-  <div className="w-72 rounded-2xl md:w-auto p-8 lg:w-4/5 relative   bg-gray-900 z-10 flex flex-col items-center justify-center h-full text-center">
+  className="w-full h-full m-auto flex justify-center align-middle my-8 sm:-mx-0 lg:-mx-0 md:w-auto md:h-96 lg:w-full lg:h-[calc(100vh-112px)]">
+  <div className="w-72 rounded-2xl md:w-auto p-8 lg:w-4/5 relative bg-gray-900 z-10 flex flex-col items-center justify-center h-full text-center">
     <h1 className="flex justify-center items-center text-2xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-6 h-48">
       {translations.hero.title}
     </h1>
